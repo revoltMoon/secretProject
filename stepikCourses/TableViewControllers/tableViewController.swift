cca6055d711327acc226f371402b14840fd5141f
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2018-04-22 14:37:21 +0300

    add comments and add search

--- stepikCourses/TableViewControllers/tableViewController.swift
+++ stepikCourses/TableViewControllers/tableViewController.swift
@@ -8,29 +8,33 @@
 
 import UIKit
 
-class tableViewController: UITableViewController, ololo, UISearchBarDelegate {
-    @IBOutlet weak var searchBar: UISearchBar!
-    
-    var filteredCourses = [String]()
-    var coursesName = [String]()
-    var imgArr = [UIImage]()
+class tableViewController: UITableViewController, ololo, UISearchResultsUpdating {
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredCourses = [String]()                       //courses after filtration
+    var coursesNamesAndImages = [String:UIImage]()         //for moment, when we will start filtrating courses(needed Img from this dict)
+    var coursesName = [String]()                           //names of courses from server
+    var imgArr = [UIImage]()                               //images of courses from server
     var stepik: stepikServer?
     var ourPage = 1
-    var isSearching = false
+    var isSearching = false                                 //searchMode
     override func viewDidLoad() {
         super.viewDidLoad()
         stepik = stepikServer()
         stepik?.delegate = self
         stepik?.getCourse(int: ourPage)
-        tableView.delegate = self
-        tableView.dataSource = self
-        searchBar.delegate = self
-        searchBar.returnKeyType = UIReturnKeyType.done
+        self.searchContr()
+    }
+    //setup our searchController
+    func searchContr() {
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isSearching{
-            return filteredCourses.count
+        return filteredCourses.count
         }
         return coursesName.count
     }
@@ -43,28 +47,26 @@ class tableViewController: UITableViewController, ololo, UISearchBarDelegate {
             } else {
                 cell.courseName?.text = coursesName[indexPath.row]
             }
-//        cell.courseName?.text = coursesName[indexPath.row]
-        cell.courseName?.textColor = UIColor(displayP3Red: 0, green: 0, blue: 100, alpha: 0.5)
-        cell.backgroundColor = UIColor(displayP3Red: 150, green: 0, blue: 100, alpha: 0.2)
-        cell.courseImage?.backgroundColor = UIColor(displayP3Red: 0, green: 0, blue: 50, alpha: 0.2)
-            if isSearching {
-                
-            } else {
-                
-            }
-            if !imgArr.isEmpty {
+            cell.courseName?.textColor = UIColor(displayP3Red: 0, green: 0, blue: 0.4, alpha: 0.5)
+            cell.backgroundColor = UIColor(displayP3Red: 0.6, green: 0, blue: 0.5, alpha: 0.2)
+            
+            
+                //adding an image
+                if !imgArr.isEmpty {
                 if imgArr[indexPath.row].isEqual(nil) || coursesName.count > imgArr.count{
+                    //проверка, если у нас нет картинки для конкретного имени
                     self.imgArr.append(UIImage(named: "NotFound.png")!)
                 }
                 if isSearching {
-                    
+                    cell.courseImage?.image = scaleImage(img: (coursesNamesAndImages[filteredCourses[indexPath.row]])!)
+                } else {
+                    cell.courseImage?.image = scaleImage(img: imgArr[indexPath.row])
                 }
-//                cell.courseImage?.image = scaleImage(img: imgArr[indexPath.row])
             }
         }
         return cell
     }
-    
+    //resising images to 90x90
     func scaleImage(img: UIImage) -> UIImage {
         let scale: CGFloat = 0.0
         UIGraphicsBeginImageContextWithOptions(CGSize(width: 90, height: 90), true, scale)
@@ -74,18 +76,15 @@ class tableViewController: UITableViewController, ololo, UISearchBarDelegate {
         return scaledImage!
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    //get course names from server
     func getNames(array: [String]) {
         for item in array{
             self.coursesName.append(item)
             self.tableView.reloadData()
         }
+        
     }
-    
+    //get course images from server
     func getImage(array: [UIImage]) {
         for item in array{
         self.imgArr.append(item)
@@ -94,17 +93,41 @@ class tableViewController: UITableViewController, ololo, UISearchBarDelegate {
             ourPage += 1
             stepik?.getCourse(int: ourPage)
         }
+        self.addImgAndNameInDict()
         self.tableView.reloadData()
     }
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchBar.text == nil || searchBar.text == "" {
-            isSearching = false
-//            view.endEditing(true)
-            tableView.reloadData()
-        } else {
-            isSearching = true
-            filteredCourses = coursesName.filter({$0.contains(searchBar.text!)})
-            tableView.reloadData()
+    
+    //this method helps us to add pairs (NameOfCourse,Image) to the Dictionary
+    func addImgAndNameInDict() {
+        for idx in 0...imgArr.count-1 {
+            //проверка, если у нас нет картинки для конкретного имени
+            if coursesName.count > imgArr.count {
+                self.imgArr.append(UIImage(named: "NotFound.png")!)
+            }
+            coursesNamesAndImages.updateValue(imgArr[idx], forKey: coursesName[idx])
         }
     }
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            isSearching = true
+            filteredCourses = coursesName.filter { name in
+                self.coursesNamesAndImages.updateValue(self.imgArr[coursesName.index(of: name)!], forKey: name)
+                return name.lowercased().contains(searchText.lowercased())
+            }
+            
+        } else {
+            isSearching = false
+            filteredCourses = coursesName
+        }
+        tableView.reloadData()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 }
